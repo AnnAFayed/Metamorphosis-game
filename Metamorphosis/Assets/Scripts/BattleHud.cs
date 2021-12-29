9ca411721e49ccc9f22c50e455bb138fdda5db00
hzekry <96088926+hzekry@users.noreply.github.com>
2021-12-29 13:53:16 +0200

    new stuff

---
+++ Metamorphosis/Assets/Scripts/BattleHud.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleHud : MonoBehaviour
+{
+    [SerializeField] Text nameText;
+    [SerializeField] Text levelText;
+    [SerializeField] HPBar hpBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
